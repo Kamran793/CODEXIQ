e7034f951e030cb7295ff111c1422b72cfb91a13
Kamran793 <mirxakamran793@gmail.com>
2025-06-24 16:19:14 +0500

    Update user-menu.tsx

--- components/user-menu.tsx
+++ components/user-menu.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import Image from 'next/image'
 import { type Session } from '@supabase/auth-helpers-nextjs'
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import { useRouter } from 'next/navigation'
@@ -40,23 +39,10 @@ export function UserMenu({ user }: UserMenuProps) {
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button variant="ghost" className="pl-0">
-            {user?.user_metadata.avatar_url ? (
-              <Image
-                height={60}
-                width={60}
-                className="h-6 w-6 select-none rounded-full ring-1 ring-zinc-100/10 transition-opacity duration-300 hover:opacity-80"
-                src={
-                  user?.user_metadata.avatar_url
-                    ? `${user.user_metadata.avatar_url}&s=60`
-                    : ''
-                }
-                alt={user.user_metadata.name ?? 'Avatar'}
-              />
-            ) : (
-              <div className="flex h-7 w-7 shrink-0 select-none items-center justify-center rounded-full bg-muted/50 text-xs font-medium uppercase text-muted-foreground">
-                {getUserInitials(user?.user_metadata.name ?? user?.email)}
-              </div>
-            )}
+            {/* Removed avatar logic, only show initials or name */}
+            <div className="flex h-7 w-7 shrink-0 select-none items-center justify-center rounded-full bg-muted/50 text-xs font-medium uppercase text-muted-foreground">
+              {getUserInitials(user?.user_metadata.name ?? user?.email)}
+            </div>
             <span className="ml-2">{user?.user_metadata.name ?? '👋🏼'}</span>
           </Button>
         </DropdownMenuTrigger>
@@ -65,17 +51,18 @@ export function UserMenu({ user }: UserMenuProps) {
             <div className="text-xs font-medium">
               {user?.user_metadata.name}
             </div>
-            <div className="text-xs text-zinc-500">{user?.email}</div>
+            {/* Adjusting the email container to be responsive */}
+            <div className="text-xs text-zinc-500 truncate w-full">{user?.email}</div>
           </DropdownMenuItem>
           <DropdownMenuSeparator />
           <DropdownMenuItem asChild>
             <a
-              href="https://vercel.com"
+              href="https://logiqcurve.com/"
               target="_blank"
               rel="noopener noreferrer"
               className="inline-flex w-full items-center justify-between text-xs"
             >
-              Vercel Homepage
+              LogIQ Curve
               <IconExternalLink className="ml-auto h-3 w-3" />
             </a>
           </DropdownMenuItem>
